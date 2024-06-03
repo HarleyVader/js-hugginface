ad0b1098b26617cbe397bb060b69a7e6cf28451e
HarleyVader <urukkai@outlook.com>
2024-06-03 11:13:26 +0200

    0.4.6

--- server.js
+++ server.js
@@ -42,13 +42,13 @@ io.on('connection', (socket) => {
     });
 
     worker.on('message', (result) => {
-        /*
+        
         // Check if result.message is defined [ENABLE TO ERROR GENERATION WITHOUT CRASHING THE SERVER]
         if (!result || !result.message) {
             console.error('Error: result.message is undefined');
             return;
         }
-        */
+        
         // If the result is an image, generate a URL for it
         if (result.message.endsWith('.png')) {
             const imageName = result.message;
