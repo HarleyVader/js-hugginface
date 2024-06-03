480d199b871da00ca94c8c2ffeb55207b495e876
HarleyVader <urukkai@outlook.com>
2024-06-03 06:34:31 +0200

    0.3.9

--- server.js
+++ server.js
@@ -18,6 +18,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Serve images from the "images" directory
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+app.get('/images', async (req, res) => {
+    const directoryPath = path.join(__dirname, 'images');
+    const files = await fs.readdir(directoryPath);
+    let html = '<html><body>';
+    files.forEach(file => {
+        if (file.endsWith('.png') || file.endsWith('.jpg') || file.endsWith('.jpeg')) {
+            html += `<img src="/images/${file}" width="64" height="64" />`;
+        }
+    });
+    html += '</body></html>';
+    res.send(html);
+});
+
 io.on('connection', (socket) => {
     console.log(`New client connected with socket ID: ${socket.id}`); // Log the socket id
 
