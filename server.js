d7625cd77a7fc2df0a0d9024872e6c4f89d7a72d
HarleyVader <urukkai@outlook.com>
2024-06-19 23:57:10 +0200

    0.8.599

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { LMStudioClient } = require('@lmstudio/sdk');
 const express = require('express');
 const WebSocket = require('ws');
 const path = require('path');
+const https = require('https');
 const fs = require('fs').promises;
 const app = express();
 const server = require('http').createServer(app);
